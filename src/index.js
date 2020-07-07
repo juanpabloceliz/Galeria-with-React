d4a2899efb9f3a5f9150ecafab30b309f638b4ae
juanpabloceliz <juanpablo.celiz@gmail.com>
2020-07-06 22:57:17 -0300

    intentos fallidos por centralizar el estado, debería hacerlo más tranquilo y siguiendo pasos. Ordené nuevamente la estructura del proyecto, creo que está mejor así

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./assets/index.css"
-import App from "./pages/App"
+import App from "./containers/App"
 
 import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
